ab83a4b3a7c5b495c2c203efad62b148acd2677f
vi-uminber <vicky.giri@uminber.com>
2024-08-30 14:55:44 +0530

    Add Products

--- fedextrackreactui/src/components/Orders.jsx
+++ fedextrackreactui/src/components/Orders.jsx
@@ -5,6 +5,9 @@ import HP from '/images/HP-Laptop.jpg'
 import Human from '/images/human.jpg'
 import style from '/images/style.jpg'
 import tech from '/images/tech.jpg'
+import herbal from '/images/herbal.webp'
+import product from '/images/product.jpg'
+import brush from '/images/brush.webp'
 
 const Orders = () => {
   const products =[
@@ -22,6 +25,15 @@ const Orders = () => {
     },
     {
         id:5, title:'Technology', description:'This is sample description', price: 500, pic:tech, trackingNumber:'070358180009382'
+    },
+    {
+        id:6, title:'Herbal Product', description:'This is sample description', price: 600, pic:herbal, trackingNumber:'797806677146'
+    },
+    {
+        id:7, title:'Daily use Product', description:'This is sample description', price: 600, pic:product, trackingNumber:'797615467620'
+    },
+    {
+        id:8, title:'Amazing Product', description:'This is sample description', price: 700, pic:brush, trackingNumber:'149331877648230'    
     },
 
   ];
